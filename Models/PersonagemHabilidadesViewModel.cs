2cfd5a3f220cdf3a14e509302b7de13ecefddff9
YAGOARAUJO1603 <y.desenvolvedor1603@gmail.com>
2023-06-06 21:39:43 -0300

    Projeto RpgMvc

---
+++ Models/PersonagemHabilidadesViewModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RpgMvc.Models
+{
+    public class PersonagemHabilidadesViewModel
+    {
+        public int PersonagemId { get; set; }
+        public PersonagemViewModel Personagem { get; set; }
+        public int HabilidadeId { get; set; }
+        public HabilidadeViewModel Habilidade { get; set; }
+        public List<PersonagemHabilidadesViewModel> PersonagemHabilidades {get; set;}
+    }
+
+    
+}
