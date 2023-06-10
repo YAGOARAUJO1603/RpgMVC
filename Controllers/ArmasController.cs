5a40c3df43b9b41d3f2ecf315790f4fd20e6eb07
YAGOARAUJO1603 <y.desenvolvedor1603@gmail.com>
2023-06-09 23:03:11 -0300

    Projeto RpgMvc

--- Controllers/ArmasController.cs
+++ Controllers/ArmasController.cs
@@ -1,12 +1,97 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RpgMvc.Models;
 
 namespace RpgMvc.Controllers
 {
-    public class ArmasController
+    public class ArmasController : Controller
     {
-        
+        public string uriBase = "http://Hugo-DS.somee.com/RpgApi/Armas/";
+
+        [HttpGet]
+        public async Task<ActionResult> IndexAsync()
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                string uriBuscaPersonagens = uriBase + "Armas/GetAll";
+                HttpResponseMessage response = await httpClient.GetAsync(uriBuscaPersonagens);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    List<ArmaViewModel> listaArmas = await Task.Run(() =>
+                        JsonConvert.DeserializeObject<List<ArmaViewModel>>(serialized));
+
+                    ViewBag.ListaAtacantes = listaArmas;
+                    ViewBag.ListaOponentes = listaArmas;
+
+                    return View();
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        [HttpGet("Delete/{habilidadeId}/{personagemId}")]
+        public async Task<ActionResult> DeleteAsync(int habilidadeId, int personagemId)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string uriComplementar = "DeletePersonagemHabilidade";
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                PersonagemHabilidadesViewModel ph = new PersonagemHabilidadesViewModel();
+                ph.HabilidadeId = habilidadeId;
+                ph.PersonagemId = personagemId;
+
+                var content = new StringContent(JsonConvert.SerializeObject(ph));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar, content);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    TempData["Mensagem"] = "Habilidade removida com sucesso";
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+            }
+            return RedirectToAction("Index", new { Id = personagemId });
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
