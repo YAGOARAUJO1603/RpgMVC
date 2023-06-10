f914b23a8f7d1c6607a4aa58b1f28378e33f63de
YAGOARAUJO1603 <y.desenvolvedor1603@gmail.com>
2023-06-10 18:58:50 -0300

    Projeto RpgMvc

--- Controllers/ArmasController.cs
+++ Controllers/ArmasController.cs
@@ -73,7 +73,67 @@ namespace RpgMvc.Controllers
                 TempData["MensagemErro"] = ex.Message;
             }
             return RedirectToAction("Index", new { Id = armaId });
+        }
 
+        [HttpGet]
+
+        public async Task<ActionResult> CreateAsync(int id, string nome)
+        {
+            try
+            {
+                string uriComplementar = "GetArmas";
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase + uriComplementar);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+                List<ArmaViewModel> armas = await Task.Run(() =>
+                JsonConvert.DeserializeObject<List<ArmaViewModel>>(serialized));
+                ViewBag.ListaArmas = armas;
+
+                ArmaViewModel ph = new ArmaViewModel();
+                ph.Personagem = new PersonagemViewModel();
+                ph.Arma = new ArmaViewModel();
+                ph.PersonagemId = id;
+                ph.Personagem.Nome = nome;
+
+                return View(ph);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Create", new { id, nome });
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync(ArmaViewModel ph)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                var content = new StringContent(JsonConvert.SerializeObject(ph));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase, content);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    TempData["Mensagem"] = "Arma cadastrada com sucesso";
+
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index", new { id = ph.PersonagemId });
+            }
+            return RedirectToAction("Index", new { id = ph.PersonagemId });
         }
 
 
